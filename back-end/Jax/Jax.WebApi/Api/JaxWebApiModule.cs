5294071d4a95937d5dbb051f824981115be5899e
shin.liu <shin820@163.com>
2017-03-17 15:29:08 +0800

    enable swagger

--- back-end/Jax/Jax.WebApi/Api/JaxWebApiModule.cs
+++ back-end/Jax/Jax.WebApi/Api/JaxWebApiModule.cs
@@ -1,9 +1,11 @@
-﻿using System.Reflection;
+﻿using System.Linq;
+using System.Reflection;
 using System.Web.Http;
 using Abp.Application.Services;
 using Abp.Configuration.Startup;
 using Abp.Modules;
 using Abp.WebApi;
+using Swashbuckle.Application;
 
 namespace Jax.Api
 {
@@ -19,6 +21,23 @@ namespace Jax.Api
                 .Build();
 
             Configuration.Modules.AbpWebApi().HttpConfiguration.Filters.Add(new HostAuthenticationFilter("Bearer"));
+
+            ConfigureSwaggerUi();
+        }
+
+        private void ConfigureSwaggerUi()
+        {
+            Configuration.Modules.AbpWebApi().HttpConfiguration
+                .EnableSwagger(c =>
+                {
+                    c.SingleApiVersion("v1", "Jax.WebApi");
+                    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                })
+                .EnableSwaggerUi(c =>
+                {
+                    c.InjectJavaScript(Assembly.GetAssembly(typeof(JaxWebApiModule)),
+                        "Jax.Scripts.Swagger-Custom.js");
+                });
         }
     }
 }
