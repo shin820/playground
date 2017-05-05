af4cd09e41f6964c69c400413f50a534723dfa36
shin820 <shin820@163.com>
2017-05-05 10:54:54 +0800

    update

--- SignalR/LiveChat/LiveChat/Hubs/ChatHub.cs
+++ SignalR/LiveChat/LiveChat/Hubs/ChatHub.cs
@@ -1,7 +1,28 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿//How to use Clients?
+//Clients.Caller
+//Clients.Others
+//Clients.Client(Context.ConnectionId)
+//Clients.AllExcept(connectionId1, connectionId2)
+//Clients.Group(groupName)
+//Clients.Group(groupName, connectionId1, connectionId2).addContosoChatMessageToPage(name, message)
+//Clients.OthersInGroup(groupName)
+//Clients.User(userid), by default userid is 'IPrincipal.Identity.Name'
+//Clients.Clients(ConnectionIds)
+//Clients.Groups(GroupIds)
+//Clients.Client(username)
+//Clients.Users(new string[] { "myUser", "myUser2" })
+
+//How to manage group membership from the Hub class?
+//Groups.Add(Context.ConnectionId, groupName);
+//Groups.Remove(Context.ConnectionId, groupName);
+/*You don't have to explicitly create groups. In effect a group is automatically created the first time you
+ *specify its name in a call to Groups.Add, and it is deleted when you remove the last connection from membership in it.
+ */
+using Microsoft.AspNet.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace LiveChat.Hubs
@@ -16,6 +37,16 @@ namespace LiveChat.Hubs
         public void Join(string name)
         {
             Clients.All.Join(name);
+        }
+
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
         }
     }
 
